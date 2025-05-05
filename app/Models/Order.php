79b30387d6aa01a1469d9627151dbf833cd19a29
fauzi ahmad <ozanfauzi39@gmail.com>
2025-05-05 15:05:17 +0700

    modal order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    public $timestamps = true;
+    protected $table = "order";
+    protected $fillable = [
+    'customer_id',
+    'total_harga',
+    'status',
+    'noresi',
+    'kurir',
+    'layanan_ongkir',
+    'biaya_ongkir',
+    'estimasi_ongkir',
+    'total_berat',
+    'alamat',
+    'pos'
+    ];
+    
+    public function orderItems()
+    {
+    return $this->hasMany(OrderItem::class);
+    }
+    public function customer()
+    {
+    return $this->belongsTo(Customer::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+    public $timestamps = true;
+    protected $table = "order_item";
+    protected $fillable = ['order_id', 'produk_id', 'quantity', 'harga'];
+
+    public function order()
+    {
+    return $this->belongsTo(Order::class);
+    }
+
+    public function produk()
+    {
+    return $this->belongsTo(Produk::class);
+    }
+    
+    public function kategori()
+    {
+    return $this->belongsTo(Kategori::class);
+    }
 }
